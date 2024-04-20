8a2734410a7dddffe6054fd45f80828ca48c5ee0
uc-steffenr <steffenr@mail.uc.edu>
2024-04-20 15:58:39 -0400

    updated port

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=80)
 
 # from flask_sqlalchemy import SQLAlchemy
 
