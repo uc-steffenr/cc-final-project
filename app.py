1d2ca1430917c19f04e5b733abbc19baea547a71
Nate Steffen <69090586+uc-steffenr@users.noreply.github.com>
2024-04-20 17:17:09 +0000

    added port

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def success():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create the database tables
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
