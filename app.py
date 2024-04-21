7aab6f55b13a61ca33d09d673f4702e410436f89
uc-steffenr <steffenr@mail.uc.edu>
2024-04-21 15:05:08 -0400

    finished part 3

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-import urllib.parse 
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-params = urllib.parse.quote_plus("")
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import max as spark_max
+from pyspark.sql.functions import min as spark_min
+from pyspark.sql.functions import col, month, year, avg, expr, collect_list, udf, stddev
+from pyspark.sql.types import DoubleType
+
+spark = SparkSession.builder \
+    .appName("cc-final-project") \
+    .config("spark.jars", "sqljdbc_12.6/enu/jars/mssql-jdbc-12.6.1.jre8.jar") \
+    .getOrCreate()
+
+df = spark.read.format("jdbc") \
+    .option("url", "jdbc:sqlserver://cc-final-sql-server.database.windows.net:1433;databaseName=kroger-data") \
+    .option("dbtable", "combined") \
+    .option("user", "final-project") \
+    .option("password", "CCPaka!@#") \
+    .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver") \
+    .load()
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # SQLite database
@@ -25,7 +42,8 @@ def register_or_signin():
         # Sign-in functionality: Check if user exists and password matches
         user = User.query.filter_by(username=username, email=email, password=password).first()
         if user:
-            return redirect(url_for('success', message="Sign-in successful!"))
+            return redirect(url_for('part_3'))
+            # return redirect(url_for('success', message="Sign-in successful!"))
         else:
             return "Invalid username, email, or password. Please try again."
 
@@ -46,13 +64,20 @@ def register():
         new_user = User(username=username, email=email, password=password)
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('success', message="User registered successfully!"))
+        return redirect(url_for('part_3'))
+        # return redirect(url_for('success', message="User registered successfully!"))
     return render_template('register.html')
 
 @app.route('/success')
 def success():
     message = request.args.get('message', 'Operation successful!')
     return message
+
+@app.route('/part_3')
+def part_3():
+    rows = df.filter(col('HSHD_NUM').contains('0010')).limit(10).collect()
+    columns = df.columns
+    return render_template('part_3.html', columns=columns, rows=rows)
 
 if __name__ == '__main__':
     with app.app_context():
--- example.py
+++ example.py
@@ -1,4 +1,8 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import max as spark_max
+from pyspark.sql.functions import min as spark_min
+from pyspark.sql.functions import col, month, year, avg, expr, collect_list, udf, stddev
+from pyspark.sql.types import DoubleType
 
 spark = SparkSession.builder \
     .appName("cc-final-project") \
@@ -14,7 +18,7 @@ households_df = spark.read.format("jdbc") \
     .load()
 
 
-print(households_df)
+# print(households_df)
 
 products_df = spark.read.format("jdbc") \
     .option("url", "jdbc:sqlserver://cc-final-sql-server.database.windows.net:1433;databaseName=kroger-data") \
@@ -25,7 +29,7 @@ products_df = spark.read.format("jdbc") \
     .load()
 
 
-print(products_df)
+# print(products_df)
 
 transactions_df = spark.read.format("jdbc") \
     .option("url", "jdbc:sqlserver://cc-final-sql-server.database.windows.net:1433;databaseName=kroger-data") \
@@ -36,5 +40,13 @@ transactions_df = spark.read.format("jdbc") \
     .load()
 
 
-print(transactions_df)
+# print(transactions_df)
 
+
+# df = households_df.join(products_df.join(transactions_df))
+
+# df.filter('HSHD_NUM')
+
+df = transactions_df.join(products_df, 'PRODUCT_NUM', 'inner')
+
+df.show()
